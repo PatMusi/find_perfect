eb0e44a7d4a15404ad01c68a92151ba6502f4d26
Patrick Musi <Patrick.Musi@bedbath.com>
2018-02-18 21:15:20 -0500

    Now generating simple standalone html files with clickable links instead of console output

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-var fs = require('fs');
+var fs = require('fs'),
+	createHTML = require('create-html'),
+	open = require('open');
 
 var domain = require('./config.js').mapDomain;
 
 var obj = JSON.parse(fs.readFileSync('input.json', 'utf8'));
 
+var pokeListHtml = '';
+
 for (var i = 0; i < obj.pokemons.length; i++) {
 	if (obj.pokemons[i].individual_attack + obj.pokemons[i].individual_defense + obj.pokemons[i].individual_stamina >= 41) {
-		console.log(Math.round(((obj.pokemons[i].individual_attack + obj.pokemons[i].individual_defense + obj.pokemons[i].individual_stamina)/45)*100) + '% ' + obj.pokemons[i].pokemon_name + ' at ' + domain + '/#' + obj.pokemons[i].latitude + ',' + obj.pokemons[i].longitude + ',18z');
+		pokeListHtml = pokeListHtml + '<div>' + Math.round(((obj.pokemons[i].individual_attack + obj.pokemons[i].individual_defense + obj.pokemons[i].individual_stamina)/45)*100) + '% ' + obj.pokemons[i].pokemon_name + ' at <a href="' + domain + '/#' + obj.pokemons[i].latitude + ',' + obj.pokemons[i].longitude + ',18z" target="_blank">' + domain + '/#' + obj.pokemons[i].latitude + ',' + obj.pokemons[i].longitude + ',18z</a></div>';
 	}
 }
+
+var html = createHTML({
+	title: 'High IV Pokemon Locations',
+	body: pokeListHtml
+});
+
+fs.writeFile('index.html', html, function(err) {
+	if (err) {
+		console.log(err);
+	}
+});
+
+
+open('./index.html');
